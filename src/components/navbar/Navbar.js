e1d927950a9b343e9e7c9b489dc361e56ce0ba3d
Gurna <g.urnieziute@gmail.com>
2023-07-08 22:34:39 +0300

    added Navbar component

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from "./styles.module.css";
+import logotypeForum from "../../assets/logotypeForum.png";
+import login from "../../assets/login.png";
+import signup from "../../assets/signup.png"
+
+const Navbar = () => {
+  return (
+    <div className={styles.navbar}>
+        <div><img className={styles.logotype} src={logotypeForum.src}/></div>
+        <div>
+            <ul>
+                <li>
+                    <a href="#">
+                        <img className={styles.icon} src={login.src}/>
+                        <div>Login</div>
+                    </a>
+                    </li>
+                <li>
+                <a href="#">
+                    <img className={styles.icon} src={signup.src}/>
+                    <div>Signup</div>
+                </a>
+                </li>
+               
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import QuestionCard from "../components/questionCard";
 import styles from "./styles.module.css";
-
+import Navbar from '../components/navbar/Navbar';
 
 
 const MainPage = () => {
@@ -11,10 +11,14 @@ const MainPage = () => {
 
   const fetchAllQuestions = async ()=>{
 
+    try{
     const response = await axios.get("http://localhost:8080/questions");
     const {data} = response
     
     setQuestions(data.question)  //duomenys ideti i setQuestions kintamaji//
+    }catch(err){
+      console.log("error", err)
+    }
   };
 
   useEffect(()=>{
@@ -24,15 +28,18 @@ const MainPage = () => {
   
   return (
     <div>
+      <Navbar/>
       <div className={styles.questionWrapper}>
         {questions.map((question)=>(
+          <div key = {question._id}>
       <QuestionCard
       id={question._id}
       title={question.title}
       date={question.date} 
       author={question.author} 
       description= {question.description}
-      />      
+      /> 
+      </div>  
       ))}
       </div>
     </div>
